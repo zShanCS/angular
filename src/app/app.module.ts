2f4cf3775bacd95860ded21b06d0f8cd8792d6a4
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2021-05-11 16:05:39 +0500

    created a Dish Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from "./services/dish.service";
  @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
