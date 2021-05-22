2f2fb05da50d0c56dfbe7d21fa4a80ad5f62521a
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2021-05-22 15:03:08 +0500

    single page application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,19 +12,31 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from "./services/dish.service";
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from "./services/dish.service";
+import { PromotionService } from "./services/promotion.service";
  @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -34,9 +46,10 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
